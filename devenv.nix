958e795b2397a652c540609fb0a7ba2b26fbb1ec
Ivan Zatevakhin <zatevaxin.ivan@gmail.com>
2024-06-23 04:22:58 +0100

    refactor: switching to poetry and devenv

--- devenv.nix
+++ devenv.nix
@@ -1,44 +1,39 @@
 { pkgs, lib, config, inputs, ... }:
-
 {
   name = "ai-assistant";
+
+  env.PYTHONPATH = ".";
+  env.OLLAMA_BASE_URL = "http://galactica.lan:11434";
 
   # https://devenv.sh/packages/
   packages = [ pkgs.git pkgs.zsh pkgs.libopus ];
 
   # https://devenv.sh/scripts/
-  # scripts.hello.exec = "echo hello from $GREET";
+  # scripts.hello.exec = "";
 
-  enterShell = ''
-
-  '';
+  enterShell = '''';
 
   # https://devenv.sh/tests/
-  enterTest = ''
-    echo "Running tests"
-    git --version | grep "2.42.0"
-  '';
-
-  # https://devenv.sh/services/
-  # services.postgres.enable = true;
+  enterTest = '''';
 
   # https://devenv.sh/languages/
   languages.python = {
     enable = true;
     version = "3.11";
 
-    venv = {
+    poetry = {
       enable = true;
-      quiet = true;
-      requirements = builtins.readFile ./requirements.txt;
+      activate.enable = true;
+      install = {
+        enable = true;
+        allExtras = true;
+      };
     };
   };
-  # https://devenv.sh/pre-commit-hooks/
-  # pre-commit.hooks.shellcheck.enable = true;
 
   # https://devenv.sh/processes/
-  processes.ping.exec = "ping example.com";
+  processes.mumble.exec = "docker compose up mumble";
 
   # See full reference at https://devenv.sh/reference/options/
-  starship.enable = true;
+  starship.enable = false;
 }
