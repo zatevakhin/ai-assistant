cfe21eef502604806e78ec4094fe35d7948c7b3b
Ivan Zatevakhin <zatevaxin.ivan@gmail.com>
2025-03-05 02:12:35 +0000

    chore: updated `devenv.nix`

--- devenv.nix
+++ devenv.nix
@@ -1,4 +1,5 @@
 {
+  config,
   pkgs,
   lib,
   ...
@@ -19,10 +20,18 @@ in {
     {
       PYTHONPATH = ".";
       OLLAMA_BASE_URL = "http://localhost:11434";
+      # OLLAMA_BASE_URL = "http://ollama.homeworld.lan";
     }
     // lib.optionalAttrs pkgs.stdenv.isLinux {
       # FIX: NixOS User problems
       LD_LIBRARY_PATH = "/run/opengl-driver/lib:$LD_LIBRARY_PATH";
+    }
+    // {
+      # NOTE: Just text colors.
+      YELLOW = "\\033[0;33m";
+      GREEN = "\\033[0;32m";
+      RED = "\\033[0;31m";
+      CLEAR = "\\033[0m";
     };
 
   # https://devenv.sh/packages/
@@ -36,7 +45,13 @@ in {
     ++ cuda;
 
   enterShell = ''
-    echo -e "Ready for development.\n"
+    echo -e "Development environment is ready!\n"
+    echo -e " - Run ${config.env.GREEN}docker compose up mumble -d${config.env.CLEAR} to spin-up Mumble server."
+    echo -e " - Run ${config.env.GREEN}docker compose up ollama -d${config.env.CLEAR} to spin-up Ollama server (if needed)."
+    echo -e " - Run ${config.env.GREEN}jupyter-ui${config.env.CLEAR} to spin-up Jupyter Notebook server."
+    echo -e " - Finally run ${config.env.GREEN}python assistant/main.py${config.env.CLEAR} to run assistant."
+    echo -e " - ${config.env.YELLOW}Don't forget${config.env.CLEAR} to cleanup running ${config.env.RED}docker containers${config.env.CLEAR} after you finish."
+    echo -e "\n"
   '';
 
   # https://devenv.sh/languages/
@@ -54,11 +69,8 @@ in {
     };
   };
 
-  # https://devenv.sh/processes/
-  processes = {
-    mumble.exec = "docker compose up mumble";
-    ollama.exec = "docker compose up ollama";
-    # TODO: Add jupyter lab web ui. (why? because i use neovim and can't use *.ipynb)
+  scripts = {
+    jupyter-ui.exec = "jupyter lab --no-browser --NotebookApp.token='' --NotebookApp.notebook_dir=$(pwd) $(pwd)/questions.ipynb";
   };
 
   # See full reference at https://devenv.sh/reference/options/
