0757ee3cb874cab241da3ba71e037eff26af263d
Ivan Zatevakhin <zatevaxin.ivan@gmail.com>
2025-03-08 06:40:35 +0000

    chore: improved a bit interrupt-or

--- assistant/components/interruptor.py
+++ assistant/components/interruptor.py
@@ -1,3 +1,4 @@
+from functools import partial
 import logging
 
 from assistant.components.llm import QueryResponse
@@ -9,7 +10,8 @@ from langchain_ollama import ChatOllama
 from .transcriber import TranscribedSegment
 from langchain.prompts import PromptTemplate
 from pydantic import BaseModel, Field
-
+from typing import Optional
+from datetime import datetime, timedelta, UTC
 
 from assistant.config import (
     ASSISTANT_NAME,
@@ -24,26 +26,26 @@ You are {ASSISTANT_NAME}, deciding how to handle a new user input while in an on
 
 Previous input: {{previous_query}}
 Current input: {{current_query}}
+Speech status: {{speech_status}}
 
 Select ONE action:
 
 1. DISCARD (Default): Continue with previous input, ignoring current input.
-   Use when current input is not urgent or interrupting.
+   Use when current input is out of place or not urgent or interrupting.
+   Appropriate when speech synthesis is still playing and the new input isn't urgent.
 
 2. INTERRUPT: Stop current process and address new input immediately.
    Use when user says WAIT, STOP, or explicitly requests interruption.
-
-3. APPEND: Combine both inputs and address together.
-   Use when current input directly relates to and enhances previous input.
-
-4. PARALLEL: Handle both inputs simultaneously.
-   Use ONLY for urgent inputs that cannot be discarded while previous remains important.
+   Also appropriate when user's input clearly contradicts or corrects what's currently being spoken.
 """
 
 class ActionDecision(BaseModel):
-    action: str = Field(description="The chosen action: DISCARD, INTERRUPT, APPEND or PARALLEL")
+    action: str = Field(description="The chosen action: DISCARD or INTERRUPT")
     reason: str = Field(description="A brief explanation for why this action was chosen")
 
+class SentencePlaying(BaseModel):
+    sentence: Sentence
+    timestamp: datetime = Field(default_factory=partial(datetime.now, UTC))
 
 class InterruptOr:
     def __init__(self, event_bus: EventBus) -> None:
@@ -58,6 +60,7 @@ class InterruptOr:
 
         self.llm = self.create_llm("llama3.2:3b")
 
+        self.now_playing: Optional[SentencePlaying] = None
         self.running = False
         self.queries: Queue[TranscribedSegment] = Queue()
         logger.info(f"{__name__} ... IDLE")
@@ -86,24 +89,37 @@ class InterruptOr:
 
             prompt = PromptTemplate(
                 template=FILTER_PROMPT_TEMPLATE,
-                input_variables=["previous_query", "current_query"],
+                input_variables=["previous_query", "current_query", "speech_status"],
             )
 
-            filter_prompt = prompt.format_prompt(previous_query=previous_query.text, current_query=query.text)
+            speech_status = "No speech currently playing"
+
+            if self.now_playing is not None:
+                dt = datetime.now(UTC) - self.now_playing.timestamp
+                ln = timedelta(milliseconds=self.now_playing.sentence.length)
+
+                speech_status = f"Currently playing {dt} of {ln} of synthesized speech"
+
+            filter_prompt = prompt.format_prompt(
+                previous_query=previous_query.text,
+                current_query=query.text,
+                speech_status=speech_status,
+            )
             result: ActionDecision = self.llm.with_structured_output(ActionDecision).invoke(filter_prompt)
 
             logger.info(f"-> {result}")
 
             if result.action in ["DISCARD"]:
                self.event_bus.publish(EventType.LLM_SENTENCE_DISCARD, query.uuid)
-               self.queries.task_done()
             elif result.action in ["INTERRUPT"]:
                self.event_bus.publish(EventType.LLM_STREAM_INTERRUPT, query.uuid)
-               self.queries.task_done()
+               self.event_bus.publish(EventType.MUMBLE_INTERRUPT_AUDIO, query.uuid)
             else:
                logger.warning(f"action = '{result.action}', is not handled")
 
+            self.queries.task_done()
         elif self.queries.empty():
+
             self.queries.put(query)
 
     def on_inference_status(self, query: str):
@@ -114,9 +130,18 @@ class InterruptOr:
 
     def on_mumble_playing_status(self, query: str):
         logger.info(f"on_mumble_playing_status('{query}')")
+        match query:
+            case "done":
+                self.now_playing = None
+                # FIXME: Removes query before all all sentences was returned.
+                if not self.queries.empty():
+                    q = self.queries.get_nowait()
+                    logger.info(f"non_interrupted({q})")
+                    self.queries.task_done()
 
     def on_mumble_now_playing(self, sentence: Sentence):
         logger.info(f"on_mumble_now_playing({sentence})")
+        self.now_playing = SentencePlaying(sentence=sentence)
 
     def on_transcription_status(self, query: str):
         logger.info(f"on_transcription_status('{query}')")
