41b1207f6db075b8de9f1880ffaf560d484d0cf7
Ivan Zatevakhin <zatevaxin.ivan@gmail.com>
2025-03-26 02:20:04 +0000

    chore: cleanup util

--- assistant/components/util.py
+++ assistant/components/util.py
@@ -1,8 +1,7 @@
 from reactivex.subject import Subject
-from typing import Callable, Any, Optional
+from typing import Callable
 from queue import Queue
 import threading
-import time
 from contextlib import contextmanager
 import logging
 from ollama import Client
@@ -26,43 +25,6 @@ def observe(q: Queue, fn: Callable) -> Subject:
 
     threading.Thread(target=producer, daemon=True).start()
     return subject
-
-def queue_as_observable(q: Queue) -> Subject:
-    subject = Subject()
-
-    def producer():
-        while True:
-            item = q.get()
-            if item is None:
-                subject.on_completed()
-                q.task_done()
-                break
-            subject.on_next(item)
-            q.task_done()
-
-    threading.Thread(target=producer, daemon=True).start()
-    return subject
-
-
-@contextmanager
-def controlled_area(callback: Callable, in_event: Any, out_event: Any, measure_time: bool = False, scope: Optional[str] = None):
-    callback(in_event)
-
-    t_start: Optional[float] = None
-    if measure_time:
-        t_start = time.perf_counter()
-
-    yield
-
-    if measure_time:
-        t_end = time.perf_counter()
-        scope = f"[{scope}] " if scope is not None else ""
-
-        logger.info(f"{scope}Execution time: {t_end - t_start:0.9f} seconds")
-
-
-    callback(out_event)
-
 
 
 @contextmanager
